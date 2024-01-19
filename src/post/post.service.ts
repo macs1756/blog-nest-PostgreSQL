b32ebb76c9329e84b849485253eabe4474c59428
macs1756 <kindrativmaksum312@gmail.com>
2024-01-19 17:37:56 +0200

    v3

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,26 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { Post } from './postSchema';
 
 @Injectable()
 export class PostService {
+  constructor(
+    @InjectRepository(Post)
+    private postRepository: Repository<Post>,
+  ) {}
+
   create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+    const newUser = this.postRepository.create(createPostDto);
+    return this.postRepository.save(newUser);
   }
 
   findAll() {
-    return `This action returns all post`;
+    return this.postRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} post`;
+    return this.postRepository.findOneBy({ id });
   }
 
-  update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
+  async update(id: number, updatePostDto: UpdatePostDto) {
+
+    
+    const updateElement = this.postRepository.findOneBy({ id });
+
+    if (updateElement) {
+      const updatedUser = { id, ...updateElement, ...updatePostDto };
+      await this.postRepository.save(updatedUser);
+      return updatedUser;
+    } else {
+      return 'Not found post';
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} post`;
+  async remove(id: number) {
+    await this.postRepository.delete(id);
+    return { message: 'User deleted successfully' };
   }
 }
